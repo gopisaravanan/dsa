3db4131e4d6b01ab7f54d1f73dff817715cd5ef0
Gopi Saravanan R <gopisaravanan1398@gmail.com>
2026-03-10 21:45:44 +0530

    find negative nums count

--- loop/findNegative.js
+++ loop/findNegative.js
@@ -14,3 +14,17 @@ const findNegativeNumbers = (arr) => {
 }
 
 console.log(findNegativeNumbers(arr))
+
+
+const findNegativeNumbersCount = (arr) => {
+    let count = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] < 0){
+            count++
+        }
+    }
+    return count;
+    
+}
+
+console.log(findNegativeNumbersCount(arr))
