8fd730753d853227e163a99e042bb38f18f6b28e
Gopi Saravanan R <gopisaravanan1398@gmail.com>
2026-03-23 22:10:12 +0530

    second largest

--- loop/secondLargest.js
+++ loop/secondLargest.js
@@ -1,13 +1,16 @@
-const arr = [3,6,1,4,9,8,2];
+const arr = [3,9,6,1,4,9,8,2,-9];
 
 const secondLargest = (arr) => {
+    if(arr.length < 2) {
+        return null;
+    }
     let largest = -Infinity;
     let secondLargest = -Infinity
     for(let i = 0; i < arr.length; i++){
         if(arr[i] > largest){
             secondLargest = largest
             largest = arr[i]
-        }else if(arr[i] > secondLargest){
+        }else if(arr[i] > secondLargest && arr[i] !== largest){
             secondLargest = arr[i]
         }
     }
