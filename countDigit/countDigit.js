5de76bd1fa03c0e3ebbfaae35b7e3f7a5a64abb6
Gopi Saravanan R <gopisaravanan1398@gmail.com>
2026-04-19 14:55:31 +0530

    update countDigit function to handle negative numbers and zero

--- countDigit/countDigit.js
+++ countDigit/countDigit.js
@@ -1,6 +1,10 @@
-const n = 2590;
+const n = -7889;
 
 const countDigit = (n) => {
+    // to handle 0
+    if (n ==0) return 1;
+    // to handle negative number
+    n= Math.abs(n)
     let count = 0;
     while(n > 0){
         n = Math.floor(n/10)
